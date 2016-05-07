a2655e48372d3391feb4b2bb0c03a55d8ef63b77
Henri Bourcereau <henri.bourcereau@gmail.com>
2016-05-07 15:13:47 +0200

    browser selection form

--- src/main.js
+++ src/main.js
@@ -1,3 +1,47 @@
-import { filterStyles } from 'cssantique'
+import { filterStyles, browsersDb } from 'cssantique'
 
-filterStyles()
+const browserNameOptions = Object.keys(browsersDb).map((name) => `<option>${name}</option>`)
+
+let styleElem = document.createElement('style')
+styleElem.innerHTML = `
+#cssantiqueForm { position: absolute; top: 2em; right: 2em; background-color: lightgray; height: 5em; width: 15em; padding:0.5em; }
+`
+
+let cssantiqueForm = document.createElement('form')
+cssantiqueForm.id = 'cssantiqueForm'
+cssantiqueForm.innerHTML = `
+  <label for='browserName'>Browser</label>
+  <select name='browserName' id='browserName'>
+  ${browserNameOptions}
+  </select><br />
+  <label for='browserVersion'>version</label>
+  <select name='browserVersion' id='browserVersion'>
+  </select><br />
+  <input type='submit' />
+  `
+window.document.body.appendChild(styleElem)
+window.document.body.appendChild(cssantiqueForm)
+
+const browserElem = document.getElementById('browserName')
+const versionElem = document.getElementById('browserVersion')
+
+updateVersions()
+browserElem.addEventListener('change', updateVersions)
+
+cssantiqueForm.addEventListener('submit',
+  function (e) {
+    e.preventDefault()
+    filterStyles({browser: {name: browserElem.value, version: versionElem.value}})
+  },
+  false
+)
+
+function updateVersions () {
+  versionElem.innerHTML = ''
+  const browserVersions = browsersDb[browserElem.value]
+  Object.keys(browserVersions).map((version) => {
+    let opt = document.createElement('option')
+    opt.text = version
+    versionElem.appendChild(opt)
+  })
+}
